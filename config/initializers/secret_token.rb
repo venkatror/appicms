e335b616db8fe87fffb99a6c8bc6f3e0d344ce47
venkatror <venkat0535@gmail.com>
2013-08-08 13:13:25 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venkatcms::Application.config.secret_token = '5ba0be3097378d6c97caef94dd045359ebef49e9bf622274f4821d60023514e1de6b10486f6795a72929394cb4754620a81202cb9c439f05834d9e29eae8fc10'
